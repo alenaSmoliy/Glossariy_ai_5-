fe367554e271b04c3b6836aac0775cd136b734e1
alenaSmoliy <smoliy.alena@yandex.ru>
2026-04-03 13:01:13 +0700

    generation_form.php

--- classes/form/generation_form.php
+++ classes/form/generation_form.php
@@ -16,7 +16,6 @@ class generation_form extends \moodleform {
         $mform->setType('course_id', PARAM_INT);
         $mform->setDefault('course_id', $COURSE->id);
         
-        // Глоссарий
         $glossaries = $DB->get_records_select_menu(
             'glossary',
             'course = ?',
@@ -34,20 +33,18 @@ class generation_form extends \moodleform {
         $mform->addElement('select', 'glossary_id', '📖 ' . get_string('select_glossary', 'local_glossary_ai'), $glossaries);
         $mform->addRule('glossary_id', null, 'required');
         
-        // Тема
         $mform->addElement('text', 'topic', '🏷️ ' . get_string('topic', 'local_glossary_ai'));
         $mform->setType('topic', PARAM_TEXT);
         $mform->addRule('topic', null, 'required');
         
-        // Количество
         $mform->addElement('select', 'terms_count', '📊 ' . get_string('terms_count', 'local_glossary_ai'), [
-            10 => '10 терминов',
-            25 => '25 терминов',
-            50 => '50 терминов'
+            10 => '10 ' . get_string('terms', 'local_glossary_ai'),
+            25 => '25 ' . get_string('terms', 'local_glossary_ai'),
+            50 => '50 ' . get_string('terms', 'local_glossary_ai')
         ]);
         $mform->setDefault('terms_count', 10);
         
-        // Язык
+        // Language selection with both Russian and English
         $mform->addElement('select', 'language', '🌐 ' . get_string('language', 'local_glossary_ai'), [
             'ru' => 'Русский',
             'en' => 'English'
