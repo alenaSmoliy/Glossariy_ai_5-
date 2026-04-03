3bb9433aaf8a4ab7c2b872b5c6eb4ad3146c0a1c
alenaSmoliy <smoliy.alena@yandex.ru>
2026-04-03 12:46:43 +0700

    version.php

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_glossary_ai';
+$plugin->version = 2026040300;
+$plugin->requires = 2022112800;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
