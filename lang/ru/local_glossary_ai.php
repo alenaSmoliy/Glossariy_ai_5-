94af12c8faea6a23c7c30422e79afc2fa44b9808
alenaSmoliy <smoliy.alena@yandex.ru>
2026-04-03 12:48:31 +0700

    local_glossary_ai.php

---
+++ lang/ru/local_glossary_ai.php
@@ -0,0 +1,28 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Глоссарий ИИ';
+$string['generate_terms'] = 'Генерация терминов';
+$string['edit_terms'] = 'Редактирование терминов';
+$string['select_glossary'] = 'Выберите глоссарий';
+$string['topic'] = 'Тема глоссария';
+$string['terms_count'] = 'Количество терминов';
+$string['language'] = 'Язык терминов';
+$string['generate_btn'] = 'Сгенерировать термины';
+$string['term'] = 'Термин';
+$string['definition'] = 'Определение';
+$string['actions'] = 'Действия';
+$string['add_to_glossary'] = 'Добавить в глоссарий';
+$string['add_all'] = 'Добавить все';
+$string['delete'] = 'Удалить';
+$string['edit'] = 'Редактировать';
+$string['add_new'] = '+ Добавить термин';
+$string['back'] = '← Назад';
+$string['new_generation'] = 'Новая генерация';
+$string['error_no_glossary'] = 'В этом курсе нет глоссариев. Сначала создайте глоссарий.';
+$string['error_generation'] = 'Ошибка при генерации терминов';
+$string['error_api'] = 'API GigaChat не настроен. Обратитесь к администратору.';
+$string['terms_added'] = 'терминов добавлено';
+$string['duplicates_skipped'] = 'дубликатов пропущено';
+$string['added_to_glossary'] = 'Добавлено в глоссарий';
+$string['glossary_ai:use'] = 'Использовать Глоссарий ИИ';
